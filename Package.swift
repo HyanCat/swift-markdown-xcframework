d6a7f46baaa279c558d862a6935b398257680725
流光不加少 <hyancat.cn@gmail.com>
2023-05-29 18:20:15 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(name: "Markdown", url: "https://pico.oss-accelerate.aliyuncs.com/upic/Markdown.xcframework.zip", checksum: "83f3e65d35fc13e2962a4b6eab4015bd338f512380f8b632a91cb80ec054a9d4"),
         .binaryTarget(name: "CAtomic", url: "https://pico.oss-accelerate.aliyuncs.com/upic/CAtomic.xcframework.zip", checksum: "ad66d7c4d7ee64d3627e4c807a1af66018f7942e30511ff7519e1d167feed012"),
-        .binaryTarget(name: "cmark_gfm", url: "https://pico.oss-accelerate.aliyuncs.com/upic/cmark_gfm.xcframework.zip", checksum: "255f2daec1c7d06f71769bc866574d552b5b231483037e4a85de724adfdac369"),
-        .binaryTarget(name: "cmark_gfm_extensions", url: "https://pico.oss-accelerate.aliyuncs.com/upic/cmark_gfm_extensions.xcframework.zip", checksum: "ace9b4ce76ecb76c9c15da98d1376e322d1b946d4df24db7764bbfe574036ccd")
+        .binaryTarget(name: "cmark_gfm", url: "https://pico.oss-accelerate.aliyuncs.com/upic/cmark_gfm.xcframework.zip", checksum: "a0d65c4ab64072b6965774e79f70bb9ebcd08b61d5d7ec2df73a9631e4bfc07e"),
+        .binaryTarget(name: "cmark_gfm_extensions", url: "https://pico.oss-accelerate.aliyuncs.com/upic/cmark_gfm_extensions.xcframework.zip", checksum: "9a91dd0e293540b499691d1481092ef3400aff9facb017b32cf58228ef04951d")
     ]
 )
