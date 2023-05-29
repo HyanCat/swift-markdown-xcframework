55e9eb1a38e2ad62464a5e4d0f17b9f6669e1869
流光不加少 <hyancat.cn@gmail.com>
2023-05-29 15:35:04 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,14 @@
 // swift-tools-version:5.7
 import PackageDescription
 
-var package = Package(
+let package = Package(
     name: "swift-markdown-xcframework",
     platforms: [.iOS(.v11), .macOS(.v10_13), .tvOS(.v11), .watchOS(.v4)],
     products: [
-      .library(
-        name: "Markdown",
-        targets: [ "Markdown" ]),
+      .library(name: "Markdown", targets: ["Markdown"]),
     ],
     dependencies: [],
     targets: [
         .binaryTarget(name: "Markdown", url: "https://github.com/HyanCat/swift-markdown/suites/13211155344/artifacts/719554546", checksum: "7825d8d9e3467417f4979a72586458ed11b12ebe7d40547d549a3a674bfc0e80")
     ]
-  ]
 )
