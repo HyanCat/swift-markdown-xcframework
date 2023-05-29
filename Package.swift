121521e014bdc85f510e49cfffa9d676fa4ad207
流光不加少 <hyancat.cn@gmail.com>
2023-05-29 16:00:16 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "Markdown", url: "https://github.com/HyanCat/swift-markdown/suites/13211155344/artifacts/719554547.zip", checksum: "7825d8d9e3467417f4979a72586458ed11b12ebe7d40547d549a3a674bfc0e80")
+        .binaryTarget(name: "Markdown", url: "https://pico.oss-accelerate.aliyuncs.com/upic/swift-markdown-xcframework.zip", checksum: "b339e2bea6491ad2f2d2b12f569ba716aeaa951e873c1301c330ee9a7b355eff")
     ]
 )
