4fd22b12525e8292961f9e0e9530dfeba1733115
流光不加少 <hyancat.cn@gmail.com>
2023-05-29 15:20:01 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,13 @@
+var package = Package(
+    name: "swift-markdown-xcframework",
+    products: [
+      .library(
+        name: "Markdown",
+        targets: [ "Markdown" ]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "Markdown", url: "https://github.com/HyanCat/swift-markdown/suites/13211155344/artifacts/719554546")
+    ]
+	]
+)
